a747d8f67d3cff244e78b0fdbbe56ce949b4694c
AKDev32 <amankumar005562@gmail.com>
2025-07-22 14:12:43 +0530

    Added DataTypes

---
+++ operators/dataTypesDemo.js
@@ -0,0 +1,21 @@
+let productPrice = 150000;
+
+let productDiscount = 15;
+
+let productName = "Apple Iphone 11 Pro max (512 GB, Blue)";
+
+let isAvailable = false;
+
+let minusZero = -0;
+
+console.log(minusZero);
+
+let x;
+
+let y = null;
+
+console.log(x);
+
+let veryBigNumber = 5743662673647364736476746476234736472323723746237462376n;
+
+console.log(veryBigNumber);
