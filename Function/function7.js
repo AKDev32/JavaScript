222448fb485cb7b75051d47d12c7684f1596e999
AKDev32 <amankumar005562@gmail.com>
2025-07-24 21:27:07 +0530

    Added functions

---
+++ Function/function7.js
@@ -0,0 +1,6 @@
+function fun(...aman) {
+  // ...value represents rest parameters
+  console.log(aman);
+}
+
+fun(1, 2, 3, 4, 5); // [1, 2, 3, 4, 5]
---
+++ Function/function8.js
@@ -0,0 +1,38 @@
+console.log(Math.abs(-10)); // 10
+// Math.abs() is a built-in function that returns the absolute value of a number
+console.log(Math.abs(10)); // 10
+// It can also be used with variables
+console.log(Math.floor(5.7)); // 5
+// Math.floor() rounds down to the nearest integer
+console.log(Math.ceil(5.1)); // 6
+// Math.ceil() rounds up to the nearest integer
+console.log(Math.round(5.5)); // 6
+// Math.round() rounds to the nearest integer, rounding up if the decimal is .5 or higher
+console.log(Math.max(1, 2, 3)); // 3
+// Math.max() returns the largest of the given numbers
+console.log(Math.min(1, 2, 3)); // 1
+// Math.min() returns the smallest of the given numbers
+console.log(Math.random()); // Random number between 0 and 1
+// Math.random() generates a random floating-point number between 0 (inclusive) and 1 (exclusive)
+console.log(Math.sqrt(16)); // 4
+// Math.sqrt() returns the square root of a number
+console.log(Math.pow(2, 3)); // 8
+// Math.pow() raises a number to the power of another number
+console.log(Math.PI); // 3.141592653589793
+// Math.PI is a constant that represents the value of π (pi)
+console.log(Math.E); // 2.718281828459045
+// Math.E is a constant that represents the base of the natural logarithm
+console.log(Math.log(10)); // 2.302585092994046
+// Math.log() returns the natural logarithm (base e) of a number
+console.log(Math.log10(100)); // 2
+// Math.log10() returns the base 10 logarithm of a number
+console.log(Math.sin(Math.PI / 2)); // 1
+// Math.sin() returns the sine of an angle (in radians)
+console.log(Math.cos(0)); // 1
+// Math.cos() returns the cosine of an angle (in radians)
+console.log(Math.tan(Math.PI / 4)); // 1
+// Math.tan() returns the tangent of an angle (in radians)
+console.log(Math.asin(1)); // 1.5707963267948966
+// Math.asin() returns the arcsine (inverse sine) of a number
+console.log(Math.acos(1)); // 0
+// Math.acos() returns the arccosine (inverse cosine) of a number
