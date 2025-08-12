1ef3ad647b7db1157518c159d383cc7eec34b856
AKDev32 <amankumar005562@gmail.com>
2025-08-12 23:46:53 +0530

    Added absOperator

---
+++ AbstractOperation/plusOperator.js
@@ -0,0 +1,7 @@
+console.log(5 + 10); // logs 15
+console.log("Hello" + 5); // logs "Hello5"
+console.log(true + 3); // logs 4
+console.log(null + 2);
+console.log([1, 2] + 3); // logs "1,23"
+console.log(undefined + 5); // logs NaN
+console.log({} + 2); // logs "[object Object]2"
