1b26936ca9635e7e152973482b4113804e36f499
AKDev32 <amankumar005562@gmail.com>
2025-08-12 23:49:55 +0530

    Added subOperator

---
+++ AbstractOperation/subOperator.js
@@ -0,0 +1,7 @@
+console.log(5 - 10); // logs -5
+console.log("Hello" - 5); // logs "Hello5"
+console.log(true - 3); // logs -2
+console.log(null - 2); // logs -2
+console.log([1, 2] - 3); // logs "1,23"
+console.log(undefined - 5); // logs NaN
+console.log({} - 2); // logs "[object Object]2"
